600abc34c9456031ec984257b4b9a1e6eaff55f4
Sandeep Kumar <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-12-05 21:54:08 +0530

    second commit

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ import subscribeRouter from './routes/subscribeRoutes.js';
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: ['https://subscription-frontend-kappa.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}
+));
 app.use(express.json());
 app.use(morgan('dev'));
 
